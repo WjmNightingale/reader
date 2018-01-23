c255560ea02f31fd584bbcf973b85f8afa9db3a2
wjm <1186619054@qq.com>
2018-01-23 16:32:05 +0800

    welcome页面

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,7 +1,19 @@
 Page({
     onTap: function (event) {
-        wx.navigateTo({
-          url: '../posts/posts',
-        });
+        // wx.navigateTo({
+        //   url: '../posts/post',
+        // });
+        wx.switchTab({
+            url: '../posts/post',
+            success: function(res){
+                // success
+            },
+            fail: function() {
+                // fail
+            },
+            complete: function() {
+                // complete
+            }
+        })
     }
 })
