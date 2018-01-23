56e5abc5d76e2046ea6c51efd672f0031b9ec73f
wjm <1186619054@qq.com>
2018-01-23 17:08:29 +0800

    文章预览页面

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,19 +1,63 @@
+//加载posts-data中的本地数据
+var postsData = require('../../data/posts-data.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    //小程序总是会读取data对象来做数据绑定，这个动作我们称为动作A
+    //而这个动作A的执行，是在onLoad函数执行之后发生的
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.setData({
+      postList: postsData.postList
+    })
   },
-
+  /**
+   * 点击swiper中的图片跳转到文章详情页
+   */
+  onSwiperTap: function(e){
+    //target 和currentTarget的区别
+    //target指的是当前点击的组件,而currentTarget 指的是捕获事件的组件
+    //target这里指的是image，而currentTarget指的是swiper
+    var postId = e.target.dataset.postid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail?id=' + postId,
+      success: function(res){
+        // success
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  },
+  /**
+   * 点击文章预览跳转到文章详情页
+   */
+  onPostTap: function(e){
+    var postId = e.currentTarget.dataset.postid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail?id=' + postId,
+      success: function(res){
+        // success
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
